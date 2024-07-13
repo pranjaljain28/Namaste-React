80d153c1f26bc0c83377f96a5c68d8e3cf3446e6
Pranjal Jain <pranjalbajaj5@gmail.com>
2024-07-14 01:25:29 +0530

    Added my project

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const heading = React.createElement(
-            "h1",
-            {id : "heading",xyz:"abc"}, 
-            "Hello World from React !"
-        );
-console.log(heading);
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "h1",{id :"child" },
+        "I'm a h1 tag"
+    )
+);
+
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
 
 
 
