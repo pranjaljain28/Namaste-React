c157604aed969bf12513d493a532298f9a0dcb45
Jain <pranjal.jain@capgemini.com>
2025-02-28 17:09:19 +0530

    Added my project

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const gps = React.createElement("div", { id: "grandparents" }, [
-  React.createElement("div", { id: "parent1" }, [
-    React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", "Happy Birthday Pranjal"),
-      React.createElement("h2", "Happy Birthday Pranjal"),
-      React.createElement("h3", "Happy Birthday Pranjal"),
+import React from "react";
+
+import ReactDOM from "react-dom/client"; 
+
+
+const gps = React.createElement("div", { id: "grandparents" }, [ 
+  React.createElement("div", { id: "parent1", key: "parent1" }, [
+    React.createElement("div", { id: "child1", key: "child1" }, [
+      React.createElement("h1", { key: "h1-pranjal" }, "Happy Birthday Pranjal"),
+      React.createElement("h2", { key: "h2-pranjal" }, "Happy Birthday Pranjal"),
+      React.createElement("h3", { key: "h3-pranjal" }, "Happy Birthday Pranjal"),
     ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", "Happy Birthday PRASUK"),
-      React.createElement("h2", "Happy Birthday PRASUK"),
-      React.createElement("h3", "Happy Birthday PRASUK"),
+    React.createElement("div", { id: "child2", key: "child2" }, [
+      React.createElement("h1", { key: "h1-prasuk" }, "Happy Birthday PRASUK"),
+      React.createElement("h2", { key: "h2-prasuk" }, "Happy Birthday PRASUK"),
+      React.createElement("h3", { key: "h3-prasuk" }, "Happy Birthday PRASUK"),
     ]),
   ]),
-  React.createElement("div", { id: "parent2" }, [
-    React.createElement("div", { id: "child3" }, [
-      React.createElement("h1", "Happy Birthday Rythm"),
-      React.createElement("h2", "Happy Birthday Rythm"),
-      React.createElement("h3", "Happy Birthday Rythm"),
+  React.createElement("div", { id: "parent2", key: "parent2" }, [
+    React.createElement("div", { id: "child3", key: "child3" }, [
+      React.createElement("h1", { key: "h1-rythm" }, "Happy Birthday Rythm"),
+      React.createElement("h2", { key: "h2-rythm" }, "Happy Birthday Rythm"),
+      React.createElement("h3", { key: "h3-rythm" }, "Happy Birthday Rythm"),
     ]),
-    React.createElement("div", { id: "child4" }, [
-      React.createElement("h1", "Happy Birthday SHRESTH"),
-      React.createElement("h2", "Happy Birthday SHRESTH"),
-      React.createElement("h3", "Happy Birthday SHRESTH"),
+    React.createElement("div", { id: "child4", key: "child4" }, [
+      React.createElement("h1", { key: "h1-shresth" }, "Happy Birthday SHRESTH"),
+      React.createElement("h2", { key: "h2-shresth" }, "Happy Birthday SHRESTH"),
+      React.createElement("h3", { key: "h3-shresth" }, "Happy Birthday SHRESTH"),
     ]),
   ]),
 ]);
@@ -55,4 +60,4 @@ console.log(heading); //js object or React element
 */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(gps);
